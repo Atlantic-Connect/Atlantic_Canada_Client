e56889cd7794defe13cf28a454a68194620d014e
Atlantic Connect <158201346+Atlantic-Connect@users.noreply.github.com>
2024-06-14 11:02:40 -0400

    Update constant.js

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://dull-teal-moose-belt.cyclic.app/api/';
+export const API_URL = 'https://atlantic-canada-server.vercel.app/api/';
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
